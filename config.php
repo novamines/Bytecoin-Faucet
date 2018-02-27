af33ec83ee490853ecddb24df989d3cc21ac5fd4
novamines <36067450+novamines@users.noreply.github.com>
2018-02-26 22:56:50 -0500

    Update config.php
    Changes to accommodate IntenseCoin.

--- config.php
+++ config.php
@@ -16,8 +16,8 @@ Bytecoin: 24gKv1yqZ2oZ7Ln4KWcL3q7D7CXibndxhDzLvrJxxw1x7tiaQ5euuduT9EkHzDta144vmw
 
 */
 
-$faucetTitle = "Bytecoin Faucet";
-$faucetSubtitle = "Earn up to 50 BCN every 12 hours.";
+$faucetTitle = "IntenseCoin Faucet";
+$faucetSubtitle = "Earn up to 50 ITNS every 12 hours.";
 $logo = "images/logo-white.png";
 
 //Faucet address for donations
@@ -26,9 +26,9 @@ $faucetAddress = "";
 //Reward time in hours
 $rewardEvery = "12";
 //Max reward and min reward as decimals Ex: Min = 10.0 & Max = 20.0
-$minReward = "6.0"; //Remember that the minimum for an eobot deposit is 5 BCN as reward.
+$minReward = "5.0"; //Remember that the minimum for an eobot deposit is 5 BCN as reward.
 $maxReward = "50.0"; 
-//Transaction fee is set to 0.01 BCN for every request.
+//Transaction fee is set to 0.01 ITNS for every request.
 
 
 //Database connection
@@ -47,7 +47,7 @@ $keys = array(
 
 //Addresses that can request more than one time but with a different payment ID.
 $clearedAddresses = array(
-	"Eobot" => "22694R3K1JvGf1m98pBsbaXCA3ULQz4xdQiYHgnNAdsVDqZDjiTH9CMj6QHhKD232wPeYtfypNzp5TX5L3NcGGSmJ8pWnPJ",
-	"Poloniex" => "25cZNQYVAi3issDCoa6fWA2Aogd4FgPhYdpX3p8KLfhKC6sN8s6Q9WpcW4778TPwcUS5jEM25JrQvjD3XjsvXuNHSWhYUsu",
-	"HitBTC" => "24zavX3Bi2PiKGWLKh4bPGTiMsn4iHf3Y6JnKCF6V1PeBpDpuwiAMZ8di7ok6B5SQT6UXUtQgusruCoXbqUZm8VJAfq2xKK"
+	//"Eobot" => "22694R3K1JvGf1m98pBsbaXCA3ULQz4xdQiYHgnNAdsVDqZDjiTH9CMj6QHhKD232wPeYtfypNzp5TX5L3NcGGSmJ8pWnPJ",
+	//"Poloniex" => "25cZNQYVAi3issDCoa6fWA2Aogd4FgPhYdpX3p8KLfhKC6sN8s6Q9WpcW4778TPwcUS5jEM25JrQvjD3XjsvXuNHSWhYUsu",
+	//"HitBTC" => "24zavX3Bi2PiKGWLKh4bPGTiMsn4iHf3Y6JnKCF6V1PeBpDpuwiAMZ8di7ok6B5SQT6UXUtQgusruCoXbqUZm8VJAfq2xKK"
 	);
